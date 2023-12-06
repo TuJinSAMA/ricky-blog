fd1834c621c3c9086bed3c972b1f54a037aef7e5
gongyuqi <gong_yu_qi@163.com>
2023-12-06 17:42:15 +0800

    connect mysql on gyq.top

--- db/index.ts
+++ db/index.ts
@@ -1,13 +1,15 @@
-import { connect } from '@planetscale/database'
-import { drizzle } from 'drizzle-orm/planetscale-serverless'
+// import { connect } from '@planetscale/database'
+import { drizzle } from 'drizzle-orm/mysql2'
+import mysql from "mysql2/promise";
 
 import { env } from '~/env.mjs'
 
 // create the connection
-const connection = connect({
+const connection = await mysql.createConnection({
   host: env.DATABASE_HOST,
-  username: env.DATABASE_USERNAME,
+  user: env.DATABASE_USERNAME,
   password: env.DATABASE_PASSWORD,
+  database: env.DATABASE_DATABASE,
 })
 
 export const db = drizzle(connection)
